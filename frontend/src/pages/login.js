7f3c3b0ec4e1315e5c592b34f794826e0b5fbfeb
Xingyu Tan <xingyu.tan@student.unsw.edu.au>
2021-04-22 03:17:11 +1000

    Update frontend/src/pages/login.js

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,64 @@
+import { Link, Redirect } from 'react-router-dom';
+import React from 'react';
+import '../App.css';
+const BASE_URL = 'http://localhost:5005';
+
+const Login = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [errorout, setErrorout] = React.useState(false);
+  const [login, setlogin] = React.useState(false);
+
+  if (document.cookie || login) {
+    return (<Redirect to="/dashboard"/>)
+  }
+  function submit () {
+    const loginPeople = {
+      email: email,
+      password: password,
+    };
+    fetch(`${BASE_URL}/admin/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(loginPeople),
+    }).then((data) => {
+      console.log('Success:', data);
+      if (data.status === 200) {
+        data.json().then(result => {
+          document.cookie = 'Token=' + result.token + '';
+          localStorage.setItem('token', result.token);
+          setlogin(true);
+        });
+      } else if (data.status === 400) {
+        setErrorout(true);
+        document.getElementById('error_out').innerText = 'Your password and email are not mactching'
+      }
+    }).catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  return (
+    <div className='App'>
+    <header className = 'App-header'>
+      <div>
+      {errorout
+        ? (<div className = 'error_pop'>
+        <button id = 'error_close' onClick={() => { setErrorout(false) }}> ❌</button>
+        <div id = 'error_out'> </div>
+        </div>)
+        : null
+      }
+      </div>
+      <h4> please Log in!  </h4>
+      Email: <input onChange={e => setEmail(e.target.value)} value = {email} type = 'text'/>
+      Password: <input onChange={e => setPassword(e.target.value)} value = {password} type = 'password'/>
+      <button to="/dashboard" className = 'button_style' onClick= {submit}>Login</button>
+      <Link to="/regist" >No Account? Regist one</Link>
+    </header>
+    </div>
+  );
+};
+export default Login;
