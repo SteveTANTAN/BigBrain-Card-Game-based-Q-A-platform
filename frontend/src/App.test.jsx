323b388b986a68b7090d86a7871a4e73019ff60b
Xingyu Tan <xingyu.tan@student.unsw.edu.au>
2021-04-23 02:03:04 +1000

    add

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router} from "react-router-dom";
 import App from './App';
+import Login from './pages/login';
+import './App.css';
+// import axios from 'axios'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("path = </>", () => {
+
+  test('test Link for session', () => {
+    render(<App />);
+
+    const inputEl = screen.getByRole('link', {  name: /have session id\? click here to play the game!/i});
+    expect(inputEl).toBeInTheDocument();
+    expect(screen.getByRole('link', {  name: /have account\? click here to login!/i})).toBeInTheDocument();
+  });
+  test('test Link for login', () => {
+    render(<App />);
+    expect(screen.getByRole('link', {  name: /have account\? click here to login!/i})).toBeInTheDocument();
+  });
+});
+
+describe("path = </login>", () => {
+
+  test('login button', () => {
+    render(
+    <Router>
+    <Login />
+    </Router>);
+
+    const inputEl = screen.getByRole('button', {  name: /login/i})
+    expect(inputEl).toBeInTheDocument();
+
+  });
+  test('login_title', () => {
+    render(
+      <Router>
+      <Login />
+      </Router>);
+    const inputEl = screen.getByRole('heading', {  name: /please log in!/i})
+    expect(inputEl).toBeInTheDocument();
+  });
 });
